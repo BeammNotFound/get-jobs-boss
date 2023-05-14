80e3083e80a6cb5cf2cd5a2925f57ba0d8cfacb7
beammnotfound <#include0613>
2023-05-15 02:52:55 +0800

    Added: Telegram bot notification

--- Readme.md
+++ Readme.md
@@ -16,11 +16,17 @@
 由于我是系统全局配置，所以这行代码没有在代码块中出现，需要的小伙伴自行添加代码配置
 
 ### 第三步：修改代码
+#### 修改主程序代码
 打开 /src/main/java/ResumeSubmission.java <br>
 修改 **baseUrl** 为自己的筛选条件 <br>
 可以在 [boss直聘](https://www.zhipin.com/hangzhou/) 中筛选你的工作条件然后复制url ⚠️ 要为&page= 结尾<br>
 **page** ：从第几页开始投递，page不能小于1<br>
-**maxPage**：投递到第几页
+**maxPage**：投递到第几页<br>
+**EnableNotifications** 为是否开启Telegram机器人通知
+#### 修改TelegramBot通知代码
+打开 /src/main/java/utils/TelegramNotificationBot.java <br>
+修改 **TELEGRAM_API_TOKEN** 为你的机器人的token <br>
+修改 **CHAT_ID** 为你的机器人的chat_id <br>
 ### 最后一步：运行代码
 需要扫码登陆
 <br>
--- src/main/java/ResumeSubmission.java
+++ src/main/java/ResumeSubmission.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.TelegramNotificationBot;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,29 +18,32 @@ import java.util.List;
  */
 @Slf4j
 public class ResumeSubmission {
+    static boolean EnableNotifications = true;
     static Integer page = 1;
     static Integer maxPage = 10;
     static String loginUrl = "https://www.zhipin.com/web/user/?ka=header-login";
     static String baseUrl = "https://www.zhipin.com/web/geek/job?query=Java&page=";
     static ChromeDriver driver = new ChromeDriver();
     static WebDriverWait wait15s = new WebDriverWait(driver, 15000);
+    static List<String> returnList = new ArrayList<>();
 
     public static void main(String[] args) {
-
         Date sdate = new Date();
         login();
-        int count = 0;
         for (int i = page; i <= maxPage; i++) {
-            log.info("=========第{}页========", i);
-            Integer integer = resumeSubmission(baseUrl + i);
-            if (integer == -1) {
+            log.info("第{}页", i);
+            if (resumeSubmission(baseUrl + i) == -1) {
                 log.info("今日沟通人数已达上限，请明天再试");
                 break;
             }
-            count += integer;
         }
         Date edate = new Date();
-        log.info("共投递{}个简历,用时{}分", count, ((edate.getTime() - sdate.getTime()) / 1000) / 60);
+        log.info("共投递{}个简历,用时{}分", returnList.size(),
+                ((edate.getTime() - sdate.getTime()) / 1000) / 60);
+
+        if (EnableNotifications) {
+            new TelegramNotificationBot().sendMessageWithList("共投递" + returnList.size() + "个简历,用时" + ((edate.getTime() - sdate.getTime()) / 1000) / 60 + "分", returnList, "Boss直聘投递");
+        }
         driver.close();
     }
 
@@ -52,7 +57,6 @@ public class ResumeSubmission {
         List<String> list = new ArrayList<>();
         driver.findElements(By.cssSelector("[class*='job-card-left']")).forEach(o -> list.add(o.getAttribute("href")));
 
-        int count = 0;
         for (String s : list) {
             driver.get(s);
 
@@ -71,20 +75,19 @@ public class ResumeSubmission {
                 }
                 String com = driver.findElements(By.className("company-info")).get(1).getText();
                 String title = driver.findElement(By.className("name")).getText();
-                log.info("投递{}公司{}一职", com, title);
-                count++;
+                returnList.add(com + " | " + title);
+                log.info("投递{}公司 | {}一职", com, title);
                 Thread.sleep(1500);
             }
         }
-        log.info("投递完毕，投递了{}个岗位", count);
-
-        return count;
+        return returnList.size();
     }
 
     @SneakyThrows
     private static void login() {
         driver.get(loginUrl);
-        wait15s.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[class*='btn-sign-switch ewm-switch']")));
+        wait15s.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[class*='btn" +
+                "-sign-switch ewm-switch']")));
         driver.findElement(By.cssSelector("[class*='btn-sign-switch ewm-switch']")).click();
         log.info("等待登陆..");
         wait15s.until(ExpectedConditions.presenceOfElementLocated(By.className("resume-catalogue")));
